b5c50da7ca69026069fc5d393ee22f0e95c40a04
chicleti2450 <leticiadomigs2450@gmail.com>
2025-09-12 10:51:28 -0300

    Sistema de Biblioteca

---
+++ app/Http/Controllers/LivroController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Livro;
+use Illuminate\Http\Request;
+
+class LivroController extends Controller
+{
+     public function store(Request $request){
+        $livro = Livro::create([
+        'titulo' => $request -> titulo,
+        'ano_publicacao'=> $request -> ano_publicacao,
+        'genero' => $request -> genero,
+        ]);
+        return response()-> json($livro);
+    }
+
+    public function index (){
+        $livros = Livro ::all();
+
+        return response()->json($livros);
+    }
+}
---
+++ app/Models/Emprestimo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Emprestimo extends Model
+{
+    use HasFactory;
+    protected $fillable =[
+        'data_devolucao',
+        'codigo_membro',
+        'codigo_livro',
+    ];
+}
